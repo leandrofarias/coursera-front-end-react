9573c28cc0243ffc4854797d14556bb2b5197800
Leandro Farias <leandro.da.cruz.farias@gmail.com>
2020-01-13 17:17:23 -0400

    Add list ul.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,16 +31,18 @@ const RenderComments = ({ comments }) => {
     return (
         <div className="col-12 col-md-5 m-1">
             <h4>Comments</h4>
-            {
-                comments.map(comment => {
-                    return (
-                        <div key={comment.id}>
-                            <p>{comment.comment}</p>
-                            <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                        </div>
-                    );
-                })
-            }
+            <ul className="list-unstyled">
+                {
+                    comments.map(comment => {
+                        return (
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
         </div>
     );
 };
