1fd7c6f8c4809e7f1dfc157ed35267be9112bac9
Leandro Farias <leandro.da.cruz.farias@gmail.com>
2020-01-15 23:48:58 -0400

    Single Page Applications Part 2.

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Contact = (props) => {
     return(
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to="/home">Home</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Contact Us</h3>
+                    <hr/>
+                </div>                
+            </div>
             <div className="row row-content">
                 <div className="col-12">
                     <h3>Location Information</h3>
@@ -26,7 +40,7 @@ const Contact = (props) => {
                         <a role="button" className="btn btn-primary" href="tel:+85212345678">
                             <i className="fa fa-phone"></i> Call
                         </a>
-                        <a role="button" className="btn btn-info" href="#">
+                        <a role="button" className="btn btn-info">
                             <i className="fa fa-skype"></i> Skype
                         </a>
                         <a role="button" className="btn btn-success" href="mailto:confusion@food.net">
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -29,7 +30,14 @@ class Main extends Component {
                 promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
                 leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
             );
-        }
+        };
+
+        const DishWithId = ({ match }) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}/>
+            );
+        };
 
         return (
             <div>
@@ -37,6 +45,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage}/>
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Route path='/menu/:dishId' component={DishWithId}/>
                     <Route exact path='/contactus' component={Contact}/>
                     <Redirect to="/home" />
                 </Switch>
